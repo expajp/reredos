d2a10daa3bca9c5fc31f24a205b2234f2a45a328
Shu OGAWARA <expajp@gmail.com>
2019-08-01 10:23:52 +0900

    modify codes to reject consecutive dots

--- lib/reredos.rb
+++ lib/reredos.rb
@@ -28,6 +28,7 @@ module Reredos
     def valid_username?(str)
       return false if str.length > USERNAME_MAX_LENGTH
       return false if str[0] == '.' || str[-1] == '.'
+      return false if str.include?('..')
       /\A[0-9a-zA-Z\.\+\-\_]+\z/ === str # 既定の文字のみで構成されている
     end
 
